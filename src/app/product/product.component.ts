7e7fbf15202d27d02f7775a7c54ad7907a7b7dff
Cédric Lefort <cedric.lefort@cnaf.fr>
2017-07-31 22:46:59 +0200

    modif de l'objet product et utilisation de vrai données en mode bouchon

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 export class Product {
-  id: number;
+  name: string;
   description: string;
-  bigramme: string;
+  codePaquet: string;
+  idAppli: string;
 }
 
 @Component({
@@ -14,10 +15,16 @@ export class Product {
 export class ProductComponent implements OnInit {
 
   products: Product[] = [
-    { id: 1, description: 'Cristal EXN', bigramme: 'CR' },
-    { id: 2, description: 'Corali', bigramme: 'OR' },
-    { id: 3, description: 'SIAS', bigramme: 'SI' },
-    { id: 3, description: 'GSFF', bigramme: 'GF' }
+    { name: 'nap', codePaquet: 'ap', description: 'Documents Dialogue pour NAP', idAppli: 'NAP' },
+    { name: 'saga', codePaquet: 'sg', description: 'Documents Dialogue pour SAGA', idAppli: 'SAGA' },
+    { name: 'nsf', codePaquet: 'sf', description: 'Documents Dialogue pour NSF', idAppli: 'NSF' },
+    { name: 'cri', codePaquet: 'cr', description: 'Documents Dialogue pour CRISTAL', idAppli: 'CRI' },
+    { name: 'magic', codePaquet: 'mg', description: 'Documents Dialogue pour MAGIC', idAppli: 'MAGIC' },
+    { name: 'csp', codePaquet: 'cp', description: 'Documents Dialogue pour CSP', idAppli: 'CSP' },
+    { name: 'npj', codePaquet: 'nj', description: 'Documents Dialogue pour NPJ', idAppli: 'NPJ' },
+    { name: 'ideal', codePaquet: 'fi', description: 'Documents Dialogue pour GFI', idAppli: 'IDEAL' },
+    { name: 'ecc', codePaquet: 'ec', description: 'Paquet did pour JUSPEN invalidité', idAppli: 'ECC' },
+    { name: 'muti', codePaquet: 'mu', description: 'Paquet did pour Mutations', idAppli: 'MUTI' }
   ];
 
   constructor() { }
