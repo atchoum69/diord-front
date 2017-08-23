3e161e9d0a037bd99e0cabafc6d4aab557831401
Cédric Lefort <cedric.lefort@cnaf.fr>
2017-08-23 20:38:49 +0200

    active le mode bouchon

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,8 @@
+// TODO : voir comment faire une configuration dev, et une configuration prod
 export class AppConfig {
 
-  public static MODE_MOCK_ENABLED = false;
+  // Flag indiquant si on active le mode "bouchon"
+  public static MODE_MOCK_ENABLED = true;
 
   // Url d'authentification a la gateway jhipster
   public static URL_API_AUTHENTIFICATION = 'http://localhost:8080/api/authenticate';
